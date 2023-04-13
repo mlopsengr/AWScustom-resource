dcb0efc04318d98749246a999914bea648be5309
Tobi John <tobioluwafemi58@gmail.com>
2023-04-13 12:05:51 +0100

    Adds first changes to app,py

---
+++ app.py
@@ -0,0 +1,16 @@
+from aws_cdk import (
+    aws_s3 as s3,
+    lambda as _lambda,
+    App, Stack
+)
+
+from my_custom_resource import MyCustomResource
+
+class OpStack(Stack):
+    def __init__(self, scope: App, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        bucket = s3.Bucket(self, "CustomResourceBucket",
+                           encryption=s3.BucketEncryption.S3_MANAGED, block_public_access=s3.BlockPublicAccess.BLOCK_ALL)
+        
+        # add another custom resource
