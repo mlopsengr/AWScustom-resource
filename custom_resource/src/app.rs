fa4772aa2fdc2c06fd3ad432588d2d9599a3710c
Tobi John <tobioluwafemi58@gmail.com>
2023-04-15 22:37:59 +0100

    adds rust app main file

---
+++ custom_resource/src/app.rs
@@ -0,0 +1,40 @@
+use aws_cdk::core::Stack;
+use aws_cdk::aws_s3::{Bucket, BlockPublicAccess, BucketEncryption};
+use ops_custom_resource::MyCustomResource;
+
+struct OpStack<'a> {
+    scope: &'a App<'a>,
+    id: &'a str,
+    props: Option<StackProps<'a>>,
+}
+
+impl<'a> Stack for OpStack<'a> {
+    fn new(scope: &'a App<'a>, id: &'a str, props: Option<StackProps<'a>>) -> Self {
+        let bucket = Bucket::new(scope, "CustomResourceBucket",
+            BucketProps {
+                encryption: BucketEncryption::S3Managed,
+                block_public_access: BlockPublicAccess::BlockAll,
+                ..Default::default()
+            }
+        );
+
+        let resource = MyCustomResource::new(scope, "MyCustomResource",
+            MyCustomResourceProps {
+                bucket_name: bucket.bucket_name,
+                ..Default::default()
+            }
+        );
+
+        OpStack {
+            scope,
+            id,
+            props,
+        }
+    }
+}
+
+fn main() {
+    let app = App::new();
+    OpStack::new(app, "CustomResourceOpstack", None);
+    app.synth();
+}
